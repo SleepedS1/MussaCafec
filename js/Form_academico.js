a03af0665d49fe1db6373e3d034e7759255d2ec2
Andres <114013148+Omandresboard@users.noreply.github.com>
2023-09-13 08:53:48 -0500

    Add files via upload

---
+++ js/Form_academico.js
@@ -0,0 +1,29 @@
+document.getElementById("mostrarFormulario1").addEventListener("click", function () {
+    document.getElementById("formulario").style.display = "block";
+    document.getElementById("formulario2").style.display = "none";
+});
+
+document.getElementById("mostrarFormulario2").addEventListener("click", function () {
+    document.getElementById("formulario").style.display = "none";
+    document.getElementById("formulario2").style.display = "block";
+});
+
+
+// Función para mostrar u ocultar las opciones adicionales según la selección
+document.getElementById('opciones').addEventListener('change', function() {
+    var opcionSeleccionada = this.value;
+
+    // Ocultar todas las opciones adicionales
+    var opcionesAdicionales = document.querySelectorAll('.hidden');
+    opcionesAdicionales.forEach(function(opcion) {
+        opcion.style.display = 'none';
+    });
+
+    // Mostrar la opción adicional correspondiente
+    if (opcionSeleccionada === 'opcion1') {
+        document.getElementById('opcion1Detalle').style.display = 'flex';
+        
+    } else if (opcionSeleccionada === 'opcion2') {
+        document.getElementById('opcion2Detalle').style.display = 'flex';
+    }
+});
